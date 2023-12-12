5cfd9f6b4a27d553fade4820c144273fb6cb33ec
imadelbouazzaoui <imad.elbouazzaoui.ee@gmail.com>
2023-12-12 23:25:11 +0100

    Adding Top App Bar

--- app/src/main/java/com/example/superheroesapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroesapp/MainActivity.kt
@@ -1,19 +1,61 @@
 package com.example.superheroesapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import com.example.superheroesapp.model.HeroesRepository
 import com.example.superheroesapp.ui.theme.SuperheroesAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SuperheroesAppTheme {
-                SuperHeroList(HeroesRepository.heroes)
+                Scaffold(
+                    topBar = {AppBar()}
+                ) {
+                    innerPadding ->
+                    SuperHeroList(HeroesRepository.heroes, innerPadding)
+                }
             }
         }
     }
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(){
+    TopAppBar(
+        title = {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                // Your Text composable
+                Text(
+                    "Superheroes",
+                    style = MaterialTheme.typography.displayLarge,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    )
+}
+
 
